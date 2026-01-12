7b80759003cd7a49074bd663f044da6eb63a4c41
YHat.pub <matt@yhat.pub>
2026-01-12 13:44:44 -0800

    Initial Commit

---
+++ Vera/VeraApp.swift
@@ -0,0 +1,17 @@
+//
+//  VeraApp.swift
+//  Vera
+//
+//  Created by Matt Chung on 1/12/26.
+//
+
+import SwiftUI
+
+@main
+struct VeraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
